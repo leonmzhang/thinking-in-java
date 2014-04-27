836456145af626aaefa43a8695bf73e735acbac8
unknown <leonmzhang@gmail.com>
2014-04-27 22:54:26 +0800

    m

---
+++ src/main/java/com/iflytek/lesson/exception/ExceptionCase.java
@@ -0,0 +1,13 @@
+package com.iflytek.lesson.exception;
+
+import java.io.IOException;
+
+public class ExceptionCase {
+
+	public static void main(String[] args) {
+		Error err = new Error();
+		NullPointerException npe = new NullPointerException();
+		IOException ioe = new IOException();
+	}
+
+}
